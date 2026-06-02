ce2e6d93e1cef3322915bb829288113a572414d0
christiangoblin <michaelthechristiangoblin@gmail.com>
2026-06-02 02:12:27 -0400

    Fix base path for Cloudflare Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/editor/',
+ base: '/',
   optimizeDeps: {
     exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util'],
   },
